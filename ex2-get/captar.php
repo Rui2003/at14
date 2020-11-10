5daae2ef23e79df0bcf731d2de36a403d9c6abd6
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-11-10 17:26:53 +0000

    Update captar.php, captar2.php, and 4 more files...

---
+++ ex2-get/captar.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_GET ['txt_nome'])&&isset($_GET['txt_idade'])){
+	$nome = $_GET['txt_nome']; 
+	$idade = $_GET['txt_idade'];
+	echo "Nome: " .$nome. "<br>";
+	echo "Idade : " .$idade;
+
+	//mostrar dados
+}
+else{
+	//mostra erro 
+}
+?>
+
